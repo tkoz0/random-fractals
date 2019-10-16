d5512ef9431a806d3b92fa6e9693eb88201af95c
TKoz Zero <tkozlowski1999@gmail.com>
2019-10-16 00:10:54 -0400

    compile as c++11

--- mandelbrot/build.sh
+++ mandelbrot/build.sh
@@ -1,2 +1,2 @@
-g++ -g -Wall -Werror -O3 -DFP64 mandelbrot_iterator.cpp -o mdb_double.out
-g++ -g -Wall -Werror -O3 -DFP128 mandelbrot_iterator.cpp -o mdb_quad.out -lquadmath
+g++ -g -Wall -Werror -std=c++11 -O3 -DFP64 mandelbrot_iterator.cpp -o mdb_double.out
+g++ -g -Wall -Werror -std=c++11 -O3 -DFP128 mandelbrot_iterator.cpp -o mdb_quad.out -lquadmath
