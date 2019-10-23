5f1bfc5fbee900decefe35c3649c16910c2dde68
TKoz Zero <tkozlowski1999@gmail.com>
2019-10-23 00:34:24 -0400

    reduced memory usage, added --fast-math to compiler settings

--- mandelbrot/mandelbrot_iterator.cpp
+++ mandelbrot/mandelbrot_iterator.cpp
@@ -35,113 +35,12 @@ uint32_t xpix, ypix;
 // number of iterations to render
 uint32_t iterlim;
 
-// file to output to
-char *out_file;
-
-// store the iterated values
-// if x is infinity, y stores the iteration count
-// otherwise, x and y store the complex number reached through iteration
-// the sequence is determined to diverge when it exceeds magnitude 2
-// data is ordered first by y decreasing, then by x increasing
-// the order of each is dependeng on the signs of the provided xwidth and ywidth
-complex_t *values = nullptr;
-complex_t *points = nullptr;
-
 // array for PPM image output
 uint8_t *img_bytes = nullptr;
 
-void init_arr() // array initialization
-{
-    if (!values) values = (complex_t*) malloc(xpix * ypix * sizeof(*values));
-    if (!points) points = (complex_t*) malloc(xpix * ypix * sizeof(*points));
-    assert(values && points);
-    complex_t *ptr1 = values;
-    complex_t *ptr2 = points;
-    for (uint32_t y = ypix; y--;)
-        for (uint32_t x = 0; x < xpix; ++x)
-            *(ptr1++) = *(ptr2++) = { xmin + (x * xwidth) / (xpix - 1), ymin + (y * ywidth) / (ypix - 1) };
-    if (!img_bytes) img_bytes = (uint8_t*) malloc(3 * xpix * ypix * sizeof(*img_bytes));
-}
-
-void render_ST() // single threaded renderer
-{
-    complex_t *vptr = values; // value pointer
-    complex_t *pptr = points; // point coordinates pointer
-    complex_t *vptr_end = vptr + (xpix * ypix);
-    for (; vptr < vptr_end; ++vptr, ++pptr)
-    {
-        uint64_t *vptr64 = (uint64_t*) vptr; // for storing integers
-        uint32_t iter = 0; // iteration counter
-        for (; iter < iterlim; ++iter)
-        {
-            num_t xsquare = vptr->x * vptr->x;
-            num_t ysquare = vptr->y * vptr->y;
-            if (xsquare + ysquare > 4.0) // diverged (magnitude > 2)
-            {
-                *vptr64 = (uint64_t) iter; // store iteration count
-                *(++vptr64) = -1; // ones in higher bytes
-#ifdef FP128
-                *(++vptr64) = -1;
-                *(++vptr64) = -1;
-#endif
-                break;
-            }
-            // iterate (x,y) --> (x*x-y*y,2*x*y) + (px,py)
-            *vptr = { xsquare - ysquare + pptr->x, 2.0 * vptr->x * vptr->y + pptr->y };
-            // tribrot (x,y) --> (x*x) --> (x*x*x-3*x*y*y,3*x*x*y-y*y*y) + (px,py)
-//            *vptr = { vptr->x * (xsquare - 3.0 * ysquare) + pptr->x, vptr->y * (3.0 * xsquare - ysquare) + pptr->y };
-        }
-    }
-}
-
-// color palette
-uint8_t cR[6] = {0xFF,0xFF,0xFF,0x00,0x00,0xFF};
-uint8_t cG[6] = {0x00,0x77,0xFF,0xFF,0x00,0x00};
-uint8_t cB[6] = {0x00,0x00,0x00,0x00,0xFF,0xFF};
-
-void output_data() // write PPM binary image (P6)
-{
-    printf("P6\n");
-    printf("%u %u\n",xpix,ypix);
-    printf("255\n");
-    // convert sequence data into an image
-    uint8_t *iptr = img_bytes;
-    complex_t *vptr = values, *vptr_end = values + (xpix * ypix);
-    for (; vptr < vptr_end; ++vptr)
-    {
-        uint64_t *vptr64 = (uint64_t*) vptr; // for storing integers
-        uint8_t r, g, b;
-        uint64_t iters = *vptr64;
-#ifdef FP64
-        if (vptr64[1] == (uint64_t) -1) // ones in y bytes = diverged
-#else
-#ifdef FP128
-        if (vptr64[2] == (uint64_t) -1) // first half of y bytes are all ones
-#endif
-#endif
-        {
-            r = cR[iters%6];
-            g = cG[iters%6];
-            b = cB[iters%6];
-        }
-        else r = g = b = 0; // black for points in the set
-        *(iptr++) = r;
-        *(iptr++) = g;
-        *(iptr++) = b;
-    }
-    // write ppm pixel binary data
-    fwrite(img_bytes,sizeof(*img_bytes),3*xpix*ypix,stdout);
-}
-
-void cleanup() // not actually needed because the OS frees process memory anyway
-{
-    assert(values);
-    free(values);
-    assert(points);
-    free(points);
-    assert(img_bytes);
-    free(img_bytes);
-}
+// forward declarations
+void render_ST();
+void output_data();
 
 int main(int argc, char **argv)
 {
@@ -190,14 +89,65 @@ int main(int argc, char **argv)
 
     auto time_start = std::chrono::high_resolution_clock::now();
 
-    init_arr(); // initialize array
-    render_ST(); // single threaded render
-    output_data(); // write file
-    cleanup(); // deallocate memory
+    img_bytes = (uint8_t*) malloc(3 * xpix * ypix * sizeof(*img_bytes));
+    render_ST(); // single threaded renderer
+    output_data();
+    free(img_bytes);
 
     auto time_end = std::chrono::high_resolution_clock::now();
     uint64_t time_nano = std::chrono::duration_cast<std::chrono::nanoseconds>(time_end-time_start).count();
     fprintf(stderr,"computation time: %lf\n",time_nano/1.0E9);
 
     return 0;
+}
+
+// color palette
+uint8_t cR[6] = {0xFF,0xFF,0xFF,0x00,0x00,0xFF};
+uint8_t cG[6] = {0x00,0x77,0xFF,0xFF,0x00,0x00};
+uint8_t cB[6] = {0x00,0x00,0x00,0x00,0xFF,0xFF};
+
+void render_ST() // single threaded renderer
+{
+    uint8_t *pix_ptr = img_bytes; // current pixel pointer
+    for (uint32_t y = ypix; y--;)
+        for (uint32_t x = 0; x < xpix; ++x)
+        {
+            complex_t point = { xmin + (x * xwidth) / (xpix - 1), ymin + (y * ywidth) / (ypix - 1) };
+            complex_t seq = point; // sequence iteration
+
+            uint32_t iter = 0;
+            for (; iter < iterlim; ++iter) // iterate sequence for this pixel
+            {
+                // precomputed values for optimization
+                num_t xsq = seq.x * seq.x;
+                num_t ysq = seq.y * seq.y;
+                if (xsq + ysq > 4.0) break; // diverged when magnitude > 2.0
+                // linear (x,y) --> (x,y) + (px,py)
+                //seq = { seq.x + point.x, seq.y + point.y };
+                // mandelbrot (x,y) --> (x*x-y*y,2*x*y) + (px,py)
+                seq = { xsq - ysq + point.x, 2.0 * seq.x * seq.y + point.y };
+                // tribrot (x,y) --> (x*(x*x-3*y*y),y*(3*x*x-y*y)) + (px,py)
+                //seq = { seq.x * (xsq - 3.0 * ysq) + point.x, seq.y * (3.0 * xsq - ysq) + point.y };
+                // quadbrot (x,y) --> ((x*x-y*y)^2-4*x*x*y*y,4*x*y*(x*x-y*y)) + (px,py)
+                //num_t sqdif = xsq - ysq;
+                //seq = { sqdif * sqdif - 4.0 * xsq * ysq + point.x, 4.0 * seq.x * seq.y * sqdif + point.y };
+            }
+
+            uint8_t r,g,b;
+            if (iter == iterlim) r = g = b = 0; // stays bounded (in the set)
+            else // diverged, color based on iterations
+                r = cR[iter%6], g = cG[iter%6], b = cB[iter%6];
+            *(pix_ptr++) = r;
+            *(pix_ptr++) = g;
+            *(pix_ptr++) = b;
+        }
+}
+
+void output_data() // write PPM binary image (P6)
+{
+    printf("P6\n");
+    printf("%u %u\n",xpix,ypix);
+    printf("255\n");
+    // write binary data from img_bytes array
+    fwrite(img_bytes,sizeof(*img_bytes),3*xpix*ypix,stdout);
 }
