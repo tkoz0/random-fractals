bd585ec8af56d7df285f906309c3ff70efda4b3a
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2022-10-15 11:06:23 -0400

    need a constructor in all of them

--- flame_tmp/variations.py
+++ flame_tmp/variations.py
@@ -45,72 +45,98 @@ class Variation:
         raise NotImplementedError()
 
 class VarLinear(Variation): # 0
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         return x,y
 
 class VarSinusoidal(Variation): # 1
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         return math.sin(x),math.sin(y)
 
 class VarSpherical(Variation): # 2
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r2 = _r2(x,y) + _eps
         return x/r2,y/r2
 
 class VarSwirl(Variation): # 3
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r2 = _r2(x,y)
         s,c = math.sin(r2),math.cos(r2)
         return x*s-y*c,x*c+y*s
 
 class VarHorseshoe(Variation): # 4
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y) + _eps
         return (x-y)*(x+y)/r,2.0*x*y/r
 
 class VarPolar(Variation): # 5
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         t = _theta(x,y)
         r = _r(x,y)
         return t/math.pi,r-1.0
 
 class VarHandkerchief(Variation): # 6
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y)
         t = _theta(x,y)
         return r*math.sin(t+r),r*math.cos(t-r)
 
 class VarHeart(Variation): # 7
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y)
         t = _theta(x,y)
         return r*math.sin(t*r),-r*math.cos(t*r)
 
 class VarDisc(Variation): # 8
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y)
         t = _theta(x,y)
         return t*math.sin(math.pi*r)/math.pi,t*math.cos(math.pi*r)/math.pi
 
 class VarSpiral(Variation): # 9
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y) + _eps
         t = _theta(x,y)
         return (math.cos(t)+math.sin(r))/r,(math.sin(t)-math.cos(r))/r
 
 class VarHyperbolic(Variation): # 10
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y) + _eps
         t = _theta(x,y)
         return math.sin(t)/r,r*math.cos(t)
 
 class VarDiamond(Variation): # 11
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y)
         t = _theta(x,y)
         return math.sin(t)*math.cos(r),math.cos(t)*math.sin(r)
 
 class VarEx(Variation): # 12
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = _r(x,y)
         t = _theta(x,y)
@@ -119,6 +145,8 @@ class VarEx(Variation): # 12
         return r*(p0**3+p1**3),r*(p0**3-p1**3)
 
 class VarJulia(Variation): # 13
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         t = _theta(x,y)
         sr = math.sqrt(_r(x,y))
@@ -126,6 +154,8 @@ class VarJulia(Variation): # 13
         return sr*math.cos(t/2.0+o),sr*math.sin(t/2+o)
 
 class VarBent(Variation): # 14
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         xr = x if x >= 0.0 else 2.0*x
         yr = y if y >= 0.0 else y/2.0
@@ -139,6 +169,8 @@ class VarWaves(Variation): # 15
         return x+b*math.sin(y/(c*c+_eps)),y+e*math.sin(x/(f*f+_eps))
 
 class VarFisheye(Variation): # 16
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = 2.0/(_r(x,y)+1.0)
         return r*y,r*x
@@ -151,18 +183,24 @@ class VarPopcorn(Variation): # 17
         return x+c*math.sin(math.tan(3*y)),y+f*math.sin(math.tan(3*x))
 
 class VarExponential(Variation): # 18
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         r = math.exp(x-1.0)
         a = math.pi*y
         return r*math.cos(a),r*math.sin(a)
 
 class VarPower(Variation): # 19
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         t = _theta(x,y)
         r = math.pow(_r(x,y),math.sin(t))
         return r*math.cos(t),r*math.sin(t)
 
 class VarCosine(Variation): # 20
+    def __init__(self):
+        pass
     def calc(self, x: float, y: float) -> Point:
         a = math.pi*x
         return math.cos(a)*math.cosh(y),-math.sin(a)*math.sinh(y)
