31539ea7b33394156eb6edafbd683a94c3520d45
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2022-10-18 22:32:54 -0400

    renderer that maybe works

--- flame_c/renderer.c
+++ flame_c/renderer.c
@@ -1,3 +1,7 @@
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+
 #include "jrand.h"
 #include "types.h"
 
@@ -31,4 +35,57 @@ static inline void _apply_xform_basic(iter_state_t *state, xform_t *xf)
     }
     // update point
     _apply_affine(&(xf->post_affine),&(state->x),&(state->y),state->vx,state->vy);
+}
+
+void _normalize_xform_weights(xform_t *xforms, uint32_t len)
+{
+    assert(len > 0);
+    num_t wsum = 0.0;
+    xform_t *xf, *xf2;
+    for (xf = xforms; len--; ++xf)
+        wsum += xf->weight;
+    for (xf2 = xforms; xf2 != xf; ++xf2)
+        xf2->weight /= wsum;
+}
+
+// randomly select flame based on cumulative weights
+static inline uint32_t _pick_xform(num_t *cw, jrand_t *jrand)
+{
+    uint32_t ret = 0;
+    num_t rand = jrand_next_float(jrand);
+    while (cw[ret] < rand)
+        ++ret;
+    return ret;
+}
+
+// histogram length == flame->size_x * flame->size_y
+// histogram indexed by (flame->size_x * y_pos) + x_pos
+void render_basic(flame_t *flame, uint32_t *histogram, jrand_t *jrand, uint64_t samples)
+{
+    num_t xmul = (float) flame->size_x / (flame->xmax - flame->xmin);
+    num_t ymul = (float) flame->size_y / (flame->ymax - flame->ymin);
+    _normalize_xform_weights(flame->xforms,flame->xforms_len);
+    // form cumulative weights array for random xform selection
+    num_t *cw = malloc(sizeof(*cw)*flame->xforms_len);
+    num_t s = 0.0;
+    for (uint32_t i = 0; i < flame->xforms_len; ++i)
+    {
+        s += flame->xforms[i].weight;
+        cw[i] = s;
+    }
+    cw[flame->xforms_len-1] = 1.0; // to correct for rounding error
+    iter_state_t state;
+    _biunit_rand(1.0,jrand,&(state.x),&(state.y));
+    for (uint32_t i = 0; i < SETTLE_ITERS; ++i)
+        _apply_xform_basic(&state,flame->xforms+_pick_xform(cw,jrand));
+    while (samples--)
+    {
+        _apply_xform_basic(&state,flame->xforms+_pick_xform(cw,jrand));
+        if (state.x < flame->xmin || state.x >= flame->xmax
+            || state.y < flame->ymin || state.y >= flame->ymax)
+            continue;
+        uint32_t x = (state.x - flame->xmin) * xmul;
+        uint32_t y = (state.y - flame->ymin) * ymul;
+        ++histogram[(flame->size_x*y)+x];
+    }
 }
